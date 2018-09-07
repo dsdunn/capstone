31d3d3aad44b75aac40ea3bdd574a4152ead79a5
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-07 17:38:17 -0600

    Add normalize.css to root index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App/styles.css';
+import './normalize.css';
 import App from './App/index.js';
 import registerServiceWorker from './registerServiceWorker';
 
