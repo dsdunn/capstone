adc7252ddc2b3d549c6bfb9337d8d793b593881e
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-07 16:49:28 -0600

    Create Header component

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './styles.css';
+import Header from '../Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        collecshare
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css'
+const Header = () => {
+  return (
+    <div>
+      HEADER
+    </div>
+  )
+}
+
+export default Header;
