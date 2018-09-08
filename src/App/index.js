83a89767a9a04e0fcb58abe5eb78935fd39065ec
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-07 18:09:00 -0600

    Add BrowserRouter to project

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
 import './styles.css';
 import Header from '../Header'
+import Login from '../Login'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Route exact path={'/logIn'} component={Login} />
         collecshare
       </div>
     );
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css'
+
 const Header = () => {
   return (
     <div className='header'>
       HEADER
+      <Link exact to={'/logIn'}>Login</Link>
     </div>
   )
 }
