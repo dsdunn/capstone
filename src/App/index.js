354ace5a51b4b322ddf0d80b2ad72077a20227fb
dsdunn <dsdunnmusic@gmail.com>
2018-09-08 15:38:59 -0600

    Hook up Provider

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import './styles.css';
 import Header from '../Header'
-<<<<<<< HEAD
-import Login from '../Login'
-import Signup from '../Signup'
-=======
 import Login from '../Login';
 import SignUp from '../SignUp';
->>>>>>> Write signUp handle change to update state
 
 class App extends Component {
   render() {
@@ -16,11 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Route exact path={'/logIn'} component={Login} />
-<<<<<<< HEAD
-        <Route exact path={'/Signup'} component={Signup} />
-=======
         <Route exact path={'/SignUp'} component={SignUp} />
->>>>>>> Write signUp handle change to update state
         collecshare
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import './normalize.css';
 import App from './App/index.js';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { CreateStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
+  
 registerServiceWorker();
