6e2a50ad15ee385f341b18d99a1bb6cc4c0eb8fc
dsdunn <dsdunnmusic@gmail.com>
2018-09-08 17:19:03 -0600

    Write handleSubmit for signUp

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -5,8 +5,8 @@ class SignUp extends Component {
     super();
     this.state = {
       email: '',
-      password: '',
-      'passwordConf': ''
+      password1: '',
+      'password2': ''
     };
   }
 
@@ -19,15 +19,26 @@ class SignUp extends Component {
     })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.validate(this.state)) {
+      //send to firebase .then(save response (Id) to store)
+    };
+  }
+
+  validate(email, password1, password2) {
+    return true;
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor='email'>email</label>
         <input id='email' onChange={this.handleChange}/>
-        <label htmlFor='password'>password</label>
-        <input id='password'onChange={this.handleChange}/>
-        <label htmlFor='passwordConf'>confirm password</label>
-        <input id='passwordConf' onChange={this.handleChange}/>
+        <label htmlFor='password1'>password</label>
+        <input id='password1'onChange={this.handleChange}/>
+        <label htmlFor='password2'>confirm password</label>
+        <input id='password2' onChange={this.handleChange}/>
         <button type='submit'>Sign Up</button>
       </form>
     )
