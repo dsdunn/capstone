c6f24a26acf8d7d3fdb3ba2bcddfccff1d157baa
dsdunn <dsdunnmusic@gmail.com>
2018-09-07 23:51:03 -0600

    Write sign-up skeleton

--- src/Signup/index.js
+++ src/Signup/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './styles.css';
+import React and { Component } from 'react';
 
-const Signup = () => {
-  return (
-    <div>
-      Signup plz!
-    </div>
-  )
+class SignIn extends Component () {
+  constructor() {
+    super() {
+      this.state = {
+        email: '',
+        password: ''
+      };
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor='email'>email</label>
+        <input id='email'/>
+        <label htmlFor='password'/>
+        <input id='password'/>
+        <button type='submit' onClick={this.handleChange} >Sign In</button>
+      </form>
+      )
+  }
 }
 
-export default Signup;
