5c9744dcc2458679a652c380cd3eb61bdca43340
dsdunn <dsdunnmusic@gmail.com>
2018-09-08 19:26:55 -0600

    Refactor header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../firebase';
+import { firebase } from '../firebase';
 import './styles.css';
 
-const Header = () => {
+const HeaderAuth = () => {
+  return (
+    <div className='header'>
+      HEADER
+      <button className='sign-out-btn' onClick={() => auth.doSignOut()}>Sign Out</button>
+    </div>
+  )
+  
+}
+
+const HeaderNoAuth = () => {
   return (
     <div className='header'>
       HEADER
@@ -10,5 +22,17 @@ const Header = () => {
     </div>
   )
 }
+
+
+
+const Header = () => {
+
+  // return firebase.auth.onAuthStateChanged((user) => {
+
+  return this.props.user ? HeaderAuth() : HeaderNoAuth() ;
+  // })
+}
+
+
 
 export default Header;
