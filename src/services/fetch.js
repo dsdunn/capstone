a06977cce9bfdf2577b9075be2dad7cb04a2c1f0
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-10 16:22:48 -0600

    Create GET request for user info

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -3,3 +3,7 @@ import { apiFetch } from './api'
 export const postUserInfo = user => {
   return apiFetch('/users', 'POST', user)
 }
+
+export const getUserInfo = (uid) => {
+  return apiFetch(`./users/${uid}`)
+}
