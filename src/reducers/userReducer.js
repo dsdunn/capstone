17f96477c7f0ad0391070c9e5a7d50d6a19c5858
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-10 14:08:27 -0600

    Rename userReducer, userIdReducer

--- src/reducers/userReducer.js
+++
@@ -1,9 +0,0 @@
-export const userReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SIGN_IN':
-      return action.userId;
-    case 'SIGN_OUT':
-      return ''
-    default: return state;
-  }
-}
