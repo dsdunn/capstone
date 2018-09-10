fa5cbb7722a0b3eaecfd7b6650c5432a164d97e1
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-10 15:12:47 -0600

    Create services directory

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import config from './config';
+
+export const apiFetch = (path, method = 'GET', payload = null) => {
+  return fetch(`${config.url}${path}`, {
+    method,
+    headers: {
+     'Content-Type': 'application/json',
+     'Accept': 'application/json'
+     // 'Authorization': `Token token=${config.apiKey}`
+    },
+    body: JSON.stringify(payload)
+  })
+}
---
+++ src/services/config.js
@@ -0,0 +1,4 @@
+export default {
+  apiKey: '66642069',
+  url: 'https://www.collecshare.herokuapp.com/api/v1'
+}
