4347dc2555ff59bf2f5bc2a60e1256f3067a83c4
dsdunn <dsdunnmusic@gmail.com>
2018-09-11 15:46:47 -0600

    refactor UserProfile

--- src/UserProfile/index.js
+++ src/UserProfile/index.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { getUserInfo } from '../services/fetch';
 // import './styles.css'
 
 class UserProfile extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+
+    }
   }
 
-  updatePhoto = (event) => {
-
-  }
-
-  updateBio = (event) => {
-
-  }
-
-  updateDisplayName = (event) => {
-
+  componentDidMount() {
+    // getUserInfo(this.props.user.uid);
   }
 
   render() {
@@ -27,42 +21,30 @@ class UserProfile extends Component {
       <div>
         <section className='profile-top'>
           <img className='profile-img'/>
-          <button className='update-profile-pic'>update profile pic</button>
-          <form id='photo-form'className='module hidden' onSubmit={this.updatePhoto}>
-            <input type='file'/>
-            <button>submit</button>
-          </form>
           <h3 className='profile-display-name'></h3>
-          <button className='update-profile-name'>update display name</button>
-          <form id='name-form' className='module hidden' onSubmit={this.updateDisplayName}>
-            <input placeholder='new display name'/>
-            <button>submit</button>
-          </form>
           <p className='profile-bio'></p>
-          <button className='update-bio'>update bio</button>
-          <form id='bio-form' className='module hidden' onSubmit={this.updateBio}>
-            <textarea placeholder='tell us a little about yourself and what you like to collect'/>
-            <button>submit</button>
-          </form>
+          <button className='update-bio'>edit profile</button>
         </section>
-        <section className='profile-collection'>
+        <section className='profile-collections'>
           <button className='add-collection-button'>Add a collection</button>
-          <form className='add-collection-form'>
-            <input id='collection-form-name' placeholder='name your collection'/>
-            <label for='collection-form-category'>Select a Category</label>
-            <select id='collection-form-category' required='true'>
-              <option value=''>choose a category</option>
-              <option value='coins'>Coins</option>
-              <option value='comics'>Comics</option>
-              <option value='cards'>Cards</option>
-              <option value='vinyl'>Vinyl</option>
-              <option value='other'>Other</option>
-            </select>
-          </form>
           your collections will go here
         </section>
       </div>
     )
   }
 }
-export default UserProfile;
+
+export const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
+
+
+
+
+
