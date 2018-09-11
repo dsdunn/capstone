2ae833c30d3b3002bab26b83d46e93ac98e950f1
dsdunn <dsdunnmusic@gmail.com>
2018-09-11 15:51:38 -0600

    flesh out EditProfile jsx

--- src/EditProfile/index.js
+++ src/EditProfile/index.js
@@ -8,17 +8,24 @@ class EditProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      uid: null,
+      displayName: '',
+      bio: '',
+      pic: null
     }
+  }
+
+  componentDidMount() {
+    //set state with user info from store
   }
 
   render() {
     return (
       <form id='photo-form'className='modal hidden' onSubmit={this.updatePhoto}>
         <img src=''/>
-        <input type='file'/>
-        <input placeholder='new display name'/>
-        <textarea placeholder='tell us a little about yourself and what you like to collect'/>
+        <input type='file' value={this.state.pic}/>
+        <input placeholder='new display name' value={this.state.displayName}/>
+        <textarea placeholder='tell us a little about yourself and what you like to collect' value={this.state.pic}/>
         <button>submit</button>
       </form>
     )
