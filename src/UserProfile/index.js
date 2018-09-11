2fc00db17727a1e856e1f380e8011080ded8e457
dsdunn <dsdunnmusic@gmail.com>
2018-09-11 14:11:08 -0600

    Write jsx for UserProfile component

--- src/UserProfile/index.js
+++ src/UserProfile/index.js
@@ -10,13 +10,45 @@ class UserProfile extends Component {
     this.state = {}
   }
 
+  updatePhoto = (event) => {
 
-render() {
-  return (
-    <div>
-      Sick profile!
-    </div>
-  )
-}
+  }
+
+  updateBio = (event) => {
+
+  }
+
+  updateDisplayName = (event) => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <section className='profile-top'>
+          <img className='profile-img'/>
+          <button className='update-profile-pic'>update profile pic</button>
+          <form id='photo-form'className='module hidden' onSubmit={this.updatePhoto}>
+            <input type='file'/>
+            <button>submit</button>
+          </form>
+          <h3 className='profile-display-name'></h3>
+          <button className='update-profile-name'</button>
+          <form id='name-form' className='module hidden' onSubmit={this.updateDisplayName}>
+            <input type='file'/>
+            <button>submit</button>
+          </form>
+          <p className='profile-bio'></p>
+          <button className='update-bio'>update bio</button>
+          <form id='bio-form' className='module hidden' onSubmit={this.updateBio}>
+            <input type='file'/>
+            <button>submit</button>
+          </form>
+        </section>
+        <section className='profile-collection'>
+        </section>
+      </div>
+    )
+  }
 }
 export default UserProfile;
