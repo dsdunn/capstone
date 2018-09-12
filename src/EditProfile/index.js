54fcc9872a959605f54f14333710a872b07fd095
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-11 18:35:30 -0600

    :ambulance: Create handleChange and handleSubmit

--- src/EditProfile/index.js
+++ src/EditProfile/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUserInfo } from '../services/fetch';
+import { saveAs } from 'file-saver/FileSaver';
 // import './styles.css'
 
 class EditProfile extends Component {
@@ -11,8 +12,9 @@ class EditProfile extends Component {
       uid: null,
       displayName: '',
       bio: '',
-      pic: ''
+      pic: null
     }
+    this.fileInput = React.createRef();
   }
 
   componentDidMount() {
@@ -22,15 +24,23 @@ class EditProfile extends Component {
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({
-      [name]: value
+      [name]: event.target.files ? event.target.files[0] : value
     })
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const fd = new FormData();
+    fd.append('image', this.state.pic, this.state.pic.name)
+    //send fd to backend
   }
 
   render() {
     return (
-      <form id='photo-form'className='modal hidden' onSubmit={this.updatePhoto}>
+      <form id='photo-form'className='modal hidden' onSubmit={this.handleSubmit}>
         <img src=''/>
-        <input type='file' name='pic' onChange={this.handleChange} value={this.state.pic}/>
+        <input type='file' ref={this.fileInput} name='pic' onChange={this.handleChange}/>
         <input placeholder='new display name' name='displayName' onChange={this.handleChange} value={this.state.displayName}/>
         <textarea placeholder='tell us a little about yourself and what you like to collect' name='bio' onChange={this.handleChange} value={this.state.bio}/>
         <button>submit</button>
