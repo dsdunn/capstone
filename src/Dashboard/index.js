3a8de1d295fb24a9ffa1be5fa16d3cc4c039de1f
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-13 15:19:10 -0600

    Add redux to Dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import './styles.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
               // <div className='user-link'><Link exact='true' to={'/user'}>View Profile</Link></div>
 {/*<Link exact='true' to={'/user/editprofile'}>
   <button className='update-bio'>edit profile</button>
@@ -6,3 +11,23 @@
 {/*<Link exact='true' to={'/user/addcollection'}>
   <button className='add-collection-button'>Add a collection</button>
 </Link>*/}
+
+// sign out btn, usr name, edit profile, add/edit collections, account settings, 
+
+const Dashboard = (props) => {
+  return (
+    <div>
+      Dashboard
+      <Link to={'/user/editprofile'}>Edit Profile</Link>
+      {props.user.username}
+
+
+    </div>
+  )
+}
+
+export const MapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(MapStateToProps)(Dashboard);
