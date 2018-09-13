0cf1f95fe31452fe0f4ec76ddf4a6e450ac88a10
dsdunn <dsdunnmusic@gmail.com>
2018-09-12 19:09:40 -0600

    set state of collectionsmall with userdata

--- src/CollectionSmall/index.js
+++ src/CollectionSmall/index.js
@@ -5,25 +5,30 @@ import { connect } from 'react-redux';
 
 export class CollectionSmall extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      location: '',
-      avatar: ''
+      user: {
+        username: '',
+        uid: '',
+        location: ''
+      }
     }
   }
 
   componentDidMount() {
-    // getUserInfo()
+    getUserInfo(this.props.uid)
+      .then(user => this.setState({user}))
   }
 
   render() {
     return(
       <div className='collection-small'>
         <img className='collection-avatar-small'/>
-        <p className='collection-location-small'>this.state.location</p>
-        <h3 className='collection-title-small'>this.props.collection.title</h3>
-        <p className='collection-description-small'>this.props.collection.description</p>
-        <div className='collection-category-small'></div>
+        <p className='collection-small-username'>{this.state.user.username}</p>
+        <p className='collection-location-small'>{this.state.user.location || 'earth'}</p>
+        <h3 className='collection-title-small'>{this.props.title}</h3>
+        <p className='collection-description-small'>{this.props.description}</p>
+        <div className='collection-category-small'>{this.props.category}</div>
       </div>
     )
   }
