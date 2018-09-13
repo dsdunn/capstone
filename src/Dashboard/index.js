c763c11c01cd3c0adeefa1c2ea0a17f410480c68
dsdunn <dsdunnmusic@gmail.com>
2018-09-12 19:38:33 -0600

    Hook up userProfile to the redux store

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,1 +1,8 @@
               // <div className='user-link'><Link exact='true' to={'/user'}>View Profile</Link></div>
+{/*<Link exact='true' to={'/user/editprofile'}>
+  <button className='update-bio'>edit profile</button>
+</Link>*/}
+
+{/*<Link exact='true' to={'/user/addcollection'}>
+  <button className='add-collection-button'>Add a collection</button>
+</Link>*/}
