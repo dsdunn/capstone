8e7b63e78a9d7cee776c3d55b54f882a73427cd3
dsdunn <dsdunnmusic@gmail.com>
2018-09-12 18:23:33 -0600

    Add route for CollectionsContainer

--- src/App/index.js
+++ src/App/index.js
@@ -17,7 +17,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <img className='test-img' src='https://images.pexels.com/photos/157519/pexels-photo-157519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'/>
+        {/*<img className='test-img' src='https://images.pexels.com/photos/157519/pexels-photo-157519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'/>*/}
         <Route exact path={'/user/addcollection'} component={AddCollection} />
         {/*<Route exact path={'/dashboard'} component={Dashboard} />*/}
         <Route exact path={'/landing'} component={LandingPage} />
@@ -26,6 +26,7 @@ class App extends Component {
         <Route path={'/user'} component={UserProfile} />
         <Route exact path={'/user/editprofile'} component={EditProfile} />
         <Route path={'/collection'} component={CollectionBig}/>
+        <Route path={'/collectionsContainer'} component={CollectionsContainer}/>
       </div>
     );
   }
