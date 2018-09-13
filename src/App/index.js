8c01786d72c027f855a47ee7bef1c909a6f2c44f
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-13 15:18:24 -0600

    Add background img to App

--- src/App/index.js
+++ src/App/index.js
@@ -8,9 +8,9 @@ import Signup from '../Signup';
 import EditProfile from '../EditProfile';
 import LandingPage from '../LandingPage';
 import CollectionBig from '../CollectionBig';
-// import Dashboard from '../Dashboard';
 import AddCollection from '../AddCollection';
 import CollectionsContainer from '../CollectionsContainer';
+import Dashboard from '../Dashboard';
 
 
 class App extends Component {
@@ -18,9 +18,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        {/*<img className='test-img' src='https://images.pexels.com/photos/157519/pexels-photo-157519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'/>*/}
         <Route exact path={'/user/addcollection'} component={AddCollection} />
-        {/*<Route exact path={'/dashboard'} component={Dashboard} />*/}
+        {<Route exact path={'/dashboard'} component={Dashboard} />}
         <Route exact path={'/landing'} component={LandingPage} />
         <Route exact path={'/login'} component={Login} />
         <Route exact path={'/signup'} component={Signup} />
