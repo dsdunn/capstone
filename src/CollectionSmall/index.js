a2c1b34fd2881b19d189d5c8bc7c9dbd1f23c492
dsdunn <dsdunnmusic@gmail.com>
2018-09-16 16:07:09 -0600

    Pass and destructure props to populate element

--- src/CollectionSmall/index.js
+++ src/CollectionSmall/index.js
@@ -17,7 +17,8 @@ class CollectionSmall extends Component {
   }
 
   componentDidMount() {
-    getUserInfo(this.props.uid)
+    console.log(this.props.uid)
+    getUserInfo(this.props.collection.uid)
       .then(user => this.setState({user}))
   }
 
@@ -27,15 +28,17 @@ class CollectionSmall extends Component {
   }
 
   render() {
+
+    const { title, description, category } = this.props.collection;
     return(
       <div className='collection-small'>
         <img className='collection-avatar-small'/>
         <button onClick={this.viewProfile}className='collection-small-profile-button'>view profile</button>
         <p className='collection-small-username'>{this.state.user.username}</p>
         <p className='collection-location-small'>{this.state.user.location || 'earth'}</p>
-        <h3 className='collection-title-small'>{this.props.title}</h3>
-        <p className='collection-description-small'>{this.props.description}</p>
-        <div className='collection-category-small'>{this.props.category}</div>
+        <h3 className='collection-title-small'>{title}</h3>
+        <p className='collection-description-small'>{description}</p>
+        <div className='collection-category-small'>{category}</div>
       </div>
     )
   }
