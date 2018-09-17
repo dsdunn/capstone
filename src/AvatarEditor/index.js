e1364455cb845730403cc392e701ef2861e52484
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-17 15:48:02 -0600

    Add rotate feature to avitar editor

--- src/AvatarEditor/index.js
+++ src/AvatarEditor/index.js
@@ -4,9 +4,22 @@ import AvatarEditor from 'react-avatar-editor'
 class MyEditor extends Component {
   constructor(props) {
     super(props)
+    this.state = { 
+      size: 10,
+      rotate: 0 
+    }
   }
 
   setEditorRef = (editor) => this.editor = editor
+
+  handleChange = (event) => {
+    let { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleRotate = () => {
+    this.setState({ rotate: this.state.rotate += 90 })
+  }
 
   render () {
     return (
@@ -17,11 +30,13 @@ class MyEditor extends Component {
           width={250}
           height={250}
           border={10}
-          scale={1.2}
+          scale={this.state.size / 10}
+          rotate={this.state.rotate}
+          borderRadius={150}
         />
-        <button
-          onClick={this.props.save}
-          >click me</button>
+        <input type='range' min='10' max='100' name='size' onChange={this.handleChange} value={this.state.size}/>
+        <button onClick={this.props.save} >click me</button>
+        <button name='rotate' onClick={this.handleRotate} value={this.state.rotate} onChange={this.handleChange}>rotate</button>
       </div>
     )
   }
