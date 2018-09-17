57c00b813e7f22aa8b6bd9de6d9e3e65ac5b20e8
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-17 16:21:28 -0600

    Add fetByCategory to collectionsContainer

--- src/CollectionsContainer/index.js
+++ src/CollectionsContainer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getAllCollections } from '../services/fetch';
+import { getAllCollections, getCollectionsByCategory } from '../services/fetch';
 import CollectionSmall from '../CollectionSmall';
 import './styles.css';
 
@@ -8,7 +8,7 @@ class CollectionsContainer extends Component {
     super(props);
     this.state = {
       collections: [],
-      category: 'all'
+      // category: 'all'
     }
   }
 
@@ -17,10 +17,26 @@ class CollectionsContainer extends Component {
       .then(response => this.setState({ collections: response }))
   }
 
+  handleClick = (event) => {
+    event.preventDefault()
+    let category = event.target.name;
+    this.fetchByCategory(category)
+  }
+
+  fetchByCategory = (category) => {
+    getCollectionsByCategory(category)
+      .then(response => this.setState({ collections: response }))
+  }
+
   render() {
     const list = this.state.collections.map(collection => <CollectionSmall collection={collection} />)
     return (
       <div className='collections-container'>
+      <button className='collections-container-cat-btn' name='comics' onClick={this.handleClick}>Comics</button>
+      <button className='collections-container-cat-btn' name='cards' onClick={this.handleClick}>Cards</button>
+      <button className='collections-container-cat-btn' name='coins' onClick={this.handleClick}>Coins</button>
+      <button className='collections-container-cat-btn' name='vinyl' onClick={this.handleClick}>Vinyl</button>
+      <button className='collections-container-cat-btn' name='other' onClick={this.handleClick}>Other</button>
         {list}
       </div>
     )
