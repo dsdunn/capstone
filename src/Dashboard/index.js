4087ebf0808127c3e93b90a1c2f5e1586062db7e
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-18 19:45:07 -0600

    Test dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -5,7 +5,7 @@ import { NavLink, Link, withRouter } from 'react-router-dom';
 import { auth } from '../firebase';
 import { setProfile } from '../actions';
 
-const Dashboard = (props) => {
+export const Dashboard = (props) => {
 
   const handleViewProfile = () => {
     props.setProfile(props.user);
@@ -22,21 +22,21 @@ const Dashboard = (props) => {
       <p className='dashboard-username'>Hello, {props.user.username}.</p>
       <div>
       <NavLink className='dashboard-link dashboard-view-profile' onClick={() => {handleViewProfile()}} exact to={'/user'} >View Profile</NavLink>
-      <a className='dashboard-link' onClick={getPath}>Edit Profile</a>
+      <a className='dashboard-link test3' onClick={getPath}>Edit Profile</a>
       <NavLink className='dashboard-link' to={'/user/addcollection'}>Add/Edit Collection</NavLink>
       <NavLink className='dashboard-link' to={'/user/settings'}>Account Settings</NavLink>
-      <Link className='dashboard-link' to={'/'} onClick={() => props.handleSignOut()}>Sign Out</Link>
+      <Link className='dashboard-link test4' to={'/'} onClick={() => props.handleSignOut()}>Sign Out</Link>
       </div>
     </div>
   )
 }
 
-export const MapStateToProps = (state) => ({
+export const mapStateToProps = (state) => ({
   user: state.user
 })
 
-export const MapDispatchToProps = (dispatch) => ({
+export const mapDispatchToProps = (dispatch) => ({
   setProfile: (user) => dispatch(setProfile(user))
 })
 
-export default withRouter(connect(MapStateToProps, MapDispatchToProps)(Dashboard));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Dashboard));
