292f4322b313bc2951c01155c408dd6f7f14a8dc
dsdunn <dsdunnmusic@gmail.com>
2018-09-30 19:56:13 -0600

    Refactor profileReducer to add collections

--- src/reducers/profileReducer.js
+++ src/reducers/profileReducer.js
@@ -2,6 +2,12 @@ export const profileReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_PROFILE':
       return action.profile;
+    case 'ADD_COLLECTIONS':
+      const newState = {
+        ...state,
+        collections: action.collections
+      }
+      return newState;
     default: return state;
   }
 }
