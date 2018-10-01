c4389472f598e1c08f68fbc2d217393be587121b
dsdunn <dsdunnmusic@gmail.com>
2018-09-30 18:06:21 -0600

    Refactor collectionSmall to make whole card link

--- src/CollectionSmall/index.js
+++ src/CollectionSmall/index.js
@@ -28,37 +28,40 @@ export class CollectionSmall extends Component {
     this.props.history.push('/user');
   }
 
-  viewCollectionBig = () => {
-    getCollection(this.props.collection.id)
-      .then(response => this.props.setCollection({...response, ...this.state.user}));
-    this.props.history.push('/collection');
-
+  viewCollectionBig = (event) => {
+    if(event.target.classList[0] !== 'user-link') {
+      getCollection(this.props.collection.id)
+        .then(response => this.props.setCollection({...response, ...this.state.user}));
+      this.props.history.push('/collection');
+      }
   }
 
   render() {
 
     const { title, description, category, image } = this.props.collection;
     return(
-      <div className='collection-small'>
-          <img className='collection-small-image' src={`https://collecshare.herokuapp.com/${image}`}/>
-        <div className='collection-small-top'>
-          <div className='collection-small-header'>
-            <div>
-              <a exact='true' onClick={this.viewCollectionBig}><h3 className='collection-small-title'>{title}</h3>
-              </a>
-              <div className='collection-small-category'>CATEGORY: <span className='collection-small-category-span'>{category}</span></div>
-            </div>
-            <div className={`collection-small-user-display ${this.props.hideuser ? 'hidden' : ''}` } onClick={this.viewProfile}>
+      <a onClick={this.viewCollectionBig}>
+        <div className='collection-small'>
+            <img className='collection-small-image' src={`https://collecshare.herokuapp.com/${image}`}/>
+          <div className='collection-small-top'>
+            <div className='collection-small-header'>
               <div>
-                <p className='collection-small-username'>{this.state.user.username}</p>
-                <p className='collection-small-location'>{this.state.user.location || 'earth'}</p>
+                <h3 className='collection-small-title'>{title}</h3>
+                
+                <div className='collection-small-category'>CATEGORY: <span className='collection-small-category-span'>{category}</span></div>
               </div>
-              <span><img className='collection-small-avatar' src={`https://collecshare.herokuapp.com/${this.state.user.avatar}`}/></span>
+              <div className={`user-link collection-small-user-display${this.props.hideuser ? 'hidden' : ''}` } onClick={this.viewProfile}>
+                <div>
+                  <p className='user-link collection-small-username'>{this.state.user.username}</p>
+                  <p className='user-link collection-small-location'>{this.state.user.location || 'earth'}</p>
+                </div>
+                <span><img className='user-link collection-small-avatar' src={`https://collecshare.herokuapp.com/${this.state.user.avatar}`}/></span>
+              </div>
             </div>
+            <p className='collection-small-description'>{description}</p>
           </div>
-          <p className='collection-small-description'>{description}</p>
         </div>
-      </div>
+      </a>
     )
   }
 }
