03466cd235689ee5cb70a6487238e370e52fedb7
dsdunn <dsdunnmusic@gmail.com>
2018-10-09 13:12:57 -0600

    Hook up landing page links

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import './styles.css'
+import { withRouter, Link } from 'react-router-dom';
+import './styles.css';
 
 const LandingPage = () => {
   return (
     <div className="wrapper">
       <section className="section parallax bg1">
-        <h1 className='project-name'>Collecshare</h1>
+        <Link style={{ color: 'white' }} to={'/home'}><h1 className='project-name'>Collecshare</h1></Link>
       </section>
       <section className="section static s1">
-        <h1>Collect.</h1>
+        <Link to={'/home'}><h1>Collect.</h1></Link>
       </section>
       <section className="section parallax bg2"></section>
       <section className="section static s2">
-        <h1>Share.</h1>
+        <Link to={'/home'}><h1>Share.</h1></Link>
       </section>
       <section className="section parallax bg3"></section>
       <section className="section static s3">
-        <h1>Connect.</h1>
+        <Link to={'/home'}><h1>Connect.</h1></Link>
       </section>
       <section className="section parallax bg4"></section>
       <section className="section static s4">
-        <h1>Explore.</h1>
+        <Link to={'/home'}><h1>Explore.</h1></Link>
       </section>
-      {/*<section className="section parallax bg5"></section>*/}
     </div>
   )
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
